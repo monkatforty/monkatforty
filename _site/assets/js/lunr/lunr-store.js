495173875798d3eed1a426ea6a2caa70a5f9e7f3
vidhun <sealedhermit@gmail.com>
2022-08-03 12:21:20 +0530

    cat post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,19 @@
 var store = [{
+        "title": "Trip to Sittong, an Offbeat Destination",
+        "excerpt":"This Holi I made a trip to Sittong, an Offbeat Destination near Darjeeling, with my Mom. Just a long weekend (3 days) was enough. Yet during such a short trip we enjoyed almost all the things to see in Sittong, soaking into the unadulterated nature’s bliss. Haven’t you heard of...","categories": [],
+        "tags": ["Travel"],
+        "url": "/trip-to-sittong-an-offbeat-destination-northbengal-himalayan-hamlet/",
+        "teaser": "/assets/images/sittong.jpg"
+      },{
         "title": "The Rathas/Chariots of Jagannath’s Chariot Parade/ Ratha Yatra at Puri",
-        "excerpt":"The word ‘Juggernaut’, meaning a large and heavy vehicle and also a huge, powerful, and overwhelming force, was coined in by the British when they saw the Chariot Parade or Rath Yatra at Puri, a beach town in Odisha. This festival is unfathomably grand, with millions of devotees visiting Puri...","categories": ["blog"],
+        "excerpt":"The word ‘Juggernaut’, meaning a large and heavy vehicle and also a huge, powerful, and overwhelming force, was coined in by the British when they saw the Chariot Parade or Rath Yatra at Puri, a beach town in Odisha. This festival is unfathomably grand, with millions of devotees visiting Puri...","categories": [],
         "tags": ["Festival"],
-        "url": "/blog/ratha-yatra-at-puri/",
+        "url": "/ratha-yatra-at-puri/",
         "teaser": "/assets/images/jagannaths-1.jpg"
       },{
         "title": "Hemis Monastery of Ladakh and the Hemis Festival",
-        "excerpt":"Hemis Monatery of Ladakh is the largest and the richest monastery of the Ladakh region. 45 milometers from Leh, stands this famous gompa or monastery at the Hemis Village, near the Hemis National Park where the endangered snow leopards are found. This 40 kilometres drive is something that remains etched...","categories": ["blog"],
+        "excerpt":"Hemis Monatery of Ladakh is the largest and the richest monastery of the Ladakh region. 45 milometers from Leh, stands this famous gompa or monastery at the Hemis Village, near the Hemis National Park where the endangered snow leopards are found. This 40 kilometres drive is something that remains etched...","categories": [],
         "tags": ["Festival"],
-        "url": "/blog/hemis-monatery-of-ladakh-and-the-hemis-festival/",
+        "url": "/hemis-monatery-of-ladakh-and-the-hemis-festival/",
         "teaser": "/assets/images/hemis.jpg"
       }]
